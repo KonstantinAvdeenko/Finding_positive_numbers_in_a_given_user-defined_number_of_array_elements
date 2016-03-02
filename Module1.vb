04a5f9b5a29007d36975d82b2782c0716825e17f
KonstantinAvdeenko <AvdeenkoKonstantin1995@gmail.com>
2016-03-02 15:28:04 +0400

    Add my finding positive numbers in a given user-defined number of array elements

---
+++ Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+    Sub Main()
+        Dim i, n, k, r As Integer
+        Dim a() As Integer = {-1, 1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6, 7, -7, 8, -8, 9, -9, 10, -10, 11, -11, 12, -12, 13, 18, 14, -14, 15, 16, 0}
+        Console.WriteLine("Введите количество элементов массива для счета не больше 29")
+        n = Convert.ToInt32(Console.ReadLine())
+        For i = 0 To n
+            If a(i) > 0 Then k = k + 1
+        Next i
+        Console.WriteLine("количество положительных элементов" & "   " & k)
+        'Изменение размерности массива для суммирования всех присвоенных массиву элементов'
+        ReDim Preserve a(0 To 29)
+        For i = 0 To 29
+            r = r + a(i)
+        Next
+        Console.WriteLine("сумма элементов массива после 0" & "   " & r)
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ READ_ME_ENG.md
@@ -0,0 +1,9 @@
+﻿This program is in the programming language VB,
+calculates the number of positive elements of an array from an array with statically assigned elements,
+the user must enter the number of elements of the array for the account.
+After calculating the positive elements of the array from the user-defined number of elements in this array,
+the dimension of this array becomes the original and is considered the sum of the elements of the array.
+
+The main Module1 module contains a single main function inside the Sub Main () entry point.
+- fully calculating the number of positive elements of the array of the number specified by the user
+elements of this array and calculating the sum of all the elements of this array.
